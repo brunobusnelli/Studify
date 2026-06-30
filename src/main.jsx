eaf169f35b0fed6251cd6a954e855a31f748c2be
Bruno Busnelli <112426957+brunobusnelli@users.noreply.github.com>
2026-06-29 22:54:31 -0300

    Use corrected Studify app entrypoint

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
+import StudifyApp from './StudifyApp.jsx';
 import './styles.css';
 import './responsive.css';
+import './mvp.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <StudifyApp />
   </React.StrictMode>
 );
