adb07c4bbaee0936a2c5623d2d0749ad77198bce
Bruno Busnelli <112426957+brunobusnelli@users.noreply.github.com>
2026-06-29 23:47:25 -0300

    Connect auth shell to app entrypoint

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import AuthShell from './AuthShell.jsx';
 import StudifyApp from './StudifyApp.jsx';
 import './styles.css';
 import './responsive.css';
 import './mvp.css';
+import './auth.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <StudifyApp />
+    <AuthShell>
+      <StudifyApp />
+    </AuthShell>
   </React.StrictMode>
 );
